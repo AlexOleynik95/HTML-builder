73d498e5d794d92aff3248dd9d79cdcb5216d781
Alex Oleynik <Slam1247@gmail.com>
2024-01-25 23:52:41 +0300

    fix: add correct data output

--- 03-files-in-folder/index.js
+++ 03-files-in-folder/index.js
@@ -1,35 +1,28 @@
-const fs = require('node:fs');
-const {readdir} = require('node:fs/promises');
+const fsPromises = require('node:fs/promises');
 const path = require('node:path');
 
 const dirPath = path.join(__dirname, 'secret-folder');
 
-
-try {
-  readdir(
-    dirPath,
-    {withFileTypes: true}
-  ).then(function showFiles(files) {
+(async function () {
+  try {
+    const files = await fsPromises.readdir(
+      dirPath,
+      {withFileTypes: true}
+    );
+  
     for (const file of files) {
       if (file.isDirectory()) {continue}
-      let pathObj = path.parse(file.name);
+      const filePath = path.parse(file.name);
+      const fileStats = await fsPromises.stat(
+        path.join(dirPath, file.name)
+      );
       console.log(
-        pathObj.name
-        + ' - ' + pathObj.ext.slice(1)
-        + ' - ' + getSize(path.join(dirPath, file.name))
+        filePath.name
+        + ' - ' + filePath.ext.slice(1)
+        + ' - ' + (fileStats.size / 1024).toFixed(3) + ' KB'
       );
     }
-  });
-} catch (err) {
-  console.log(err);
-}
-
-function getSize(filePath) {
-  fs.stat(filePath, (err, stats) => {
-    if (err) {
-      console.log(err);
-    } else {
-      console.log(`${(stats.size / 1024).toFixed(3)} KB`);
-    }
-  });
-}
+  } catch (err) {
+    console.log(err);
+  }
+})();
